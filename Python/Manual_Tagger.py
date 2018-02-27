a120fff101bbdb4eb284769749b221225e157e47
Adam <adn2@aber.ac.uk>
2018-02-27 18:42:21 +0000

    added boilerplate code for the Manual Tagger

--- Python/Manual_Tagger.py
+++ Python/Manual_Tagger.py
@@ -16,6 +16,11 @@
 # - Find a way to make sure "Harry Smith" and "Mr Smith" are recognised as the same person
 # - Conversations. If displaying all speech from one person, may loose context if we cant see replies
 
+import textwrap
+import sys
+import random
+from bs4 import BeautifulSoup as bs
+
 
 class ManualTagger:
 
@@ -23,10 +28,70 @@ class ManualTagger:
         print("MANUAL TAGGER CLASS CREATED")
 
         self.files = files
+        self.file_count = 0
 
-    def read_file(self):
+    def read_file(self, file):
         # find topic
         # find member
         # find all instances where topic and member are the same/similar enough (john smith == Mr. Smith)
+        # provide to speech display method
+        # get user input
+        pass
+# end class ManualTagger
+
+
+class Display:
+    options_menu = "1: Mark Sentiment\n" \
+                   "2: Edit Member\n" \
+                   "3: Edit Subject\n" \
+                   "Q: Quit\n" \
+                   ":"
+    options_dict = dict()
+
+    def __init__(self, window_width=70):
+        print("INITIALIZING DISPLAY")
+        self.width = window_width
+        self.options_dict = {"1": self.mark_sentiment,
+                             "2": self.edit_member,
+                             "3": self.edit_subject,
+                             "Q": self.quit}
+        self.move_on = False
+
+    def display_speeches(self, member, topic, speeches):
+        pass
+        print("Member: ", member)
+        print("Topic:  ", topic)
+        i = 1
+        for speech in speeches:
+            print("Speech ", i, ":")
+            print(textwrap.wrap(speech, self.width))
+
+    def display_menu(self):
+        while not self.move_on:
+            try:
+                new_input = input(self.options_menu).upper()
+                self.options_dict[new_input]()
+            except KeyError:
+                print("{} NOT VALID INPUT".format(new_input))
+
+    def mark_sentiment(self):
+        print("MARK SENTIMENT")
+        self.move_on = True
         pass
 
+    def edit_member(self):
+        print("EDIT MEMBER")
+
+    def edit_subject(self):
+        print("EDIT SUBJECT")
+
+    def quit(self):
+        quit_input = input("Are You Sure you want to quit? Y/N: ").upper()
+        if quit_input == 'Y':
+            quit()
+# end class Display
+
+
+print("Running: ", sys.argv[0])
+display = Display()
+display.display_menu()
