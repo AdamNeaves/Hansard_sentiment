a8e868a480d848cf5c35e3f149feebb829ae15a4
Adam Neaves <adn2@aber.ac.uk>
2018-05-04 12:07:44 +0100

    Added some framework for testing the Parser

--- Python/tests/test_Parser.py
+++ Python/tests/test_Parser.py
@@ -1,13 +1,31 @@
 import unittest
 import Speech_Parser_BS
+import os
+from lxml.doctestcompare import LXMLOutputChecker
+import lxml
+from doctest import Example
 
 
 class SpeechParserTest(unittest.TestCase):
 
     def setUp(self):
         self.Parser = Speech_Parser_BS.SpeechParser("./Test Data/Source Data/Test File")
+        self.Parser.find_files()
 
     def tearDown(self):
-        pass
+        directory = ".\Test Data\Parsed Speech\Test File"
+        for file in os.listdir(directory):
+            os.remove(os.path.join(directory,file))
+        os.rmdir(directory)
+
+    def test_parser_find_files(self):
+        expected_array = ["test_file.xml"]
+        self.assertEqual(expected_array, self.Parser.files)
+
+    @unittest.skip("Unable to find way to compare XML trees")
+    def test_parser_parse_files(self):
+
+        self.Parser.parse_files()
+        self.assertEqual()
 
 
